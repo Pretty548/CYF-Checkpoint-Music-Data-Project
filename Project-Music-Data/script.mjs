dbf6d01a80cd0bedf3e60f7a52a6eac9e2d092eb
Pretty Taruvinga <pretty548@gmail.com>
2026-06-09 18:11:23 +0200

    populate user dropdown using getUserIDs

--- Project-Music-Data/script.mjs
+++ Project-Music-Data/script.mjs
@@ -3,9 +3,22 @@
 // You can delete the contents of the file once you have understood how it works.
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
+import { getUserIDs } from "./data.js";
 
-import { countUsers } from "./common.mjs";
+window.onload = () => {
+  const select = document.getElementById("user-select");
 
-window.onload = function () {
-  document.querySelector("body").innerText = `There are ${countUsers()} users`;
+  getUserIDs().forEach((userID) => {
+    const option = document.createElement("option");
+
+    option.value = userID;
+    option.textContent = `User ${userID}`;
+
+    select.appendChild(option);
+  });
+
+  select.addEventListener("change", (event) => {
+    const userID = event.target.value;
+    console.log("Selected user ID:", userID);
+  });
 };
